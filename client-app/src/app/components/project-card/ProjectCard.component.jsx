bd3f6fdf1d0d9c1e4cff3b8881b33e6316ed8e80
Cujbă George Valentin <george.cujba15@gmail.com>
2024-03-06 23:50:39 +0200

    Update Styles

--- client-app/src/app/components/project-card/ProjectCard.component.jsx
+++ client-app/src/app/components/project-card/ProjectCard.component.jsx
@@ -7,35 +7,36 @@ const ProjectCard = ({ project }) => {
     project;
 
   return (
-    <div className="card gap-2">
-      <div className="card-body">
-        <h5 className="card-title text-center fs-5 fw-bolder">{name}</h5>
-        <div className="card-text d-flex flex-column text-center mb-2">
-          <span className="fw-bolder">Description:</span>{" "}
-          <span className="project-description">{description}</span>
+    <div className='card gap-2 w-75'>
+      <div className='card-body'>
+        <h5 className='card-title text-center fs-5 fw-bolder'>{name}</h5>
+
+        <div className='card-text d-flex flex-column text-center mb-2'>
+          <span className='fw-bolder'>Description:</span>{" "}
+          <span className='project-description text-center'>{description}</span>
         </div>
-        <div className="card-text d-flex flex-column">
-          <span className="text-center fw-bolder">Project Type:</span>{" "}
-          <span className="text-center fst-italic">{projectType}</span>
+        <div className='card-text d-flex flex-column'>
+          <span className='text-center fw-bolder'>Project Type:</span>{" "}
+          <span className='text-center fst-italic'>{projectType}</span>
         </div>
       </div>
 
       <TechStack techStack={techStack} />
 
-      <div className="view-app-container d-flex justify-content-center align-items-center gap-4 mb-2">
+      <div className='view-app-container d-flex justify-content-center align-items-center gap-4 mb-2'>
         {!appLink ? (
-          <div className="fw-bold border border-1 border-secondary rounded p-1">
+          <div className='fw-bold border border-1 border-secondary rounded p-1'>
             Not hosted!
           </div>
         ) : (
           <HyperLink
             hrefLink={appLink}
-            logoValue="../../../../images/view.png"
+            logoValue='../../../../images/view.png'
           />
         )}
         <HyperLink
           hrefLink={codeRepoLink}
-          logoValue="../../../../images/github.png"
+          logoValue='../../../../images/github.png'
         />
       </div>
     </div>
--- client-app/src/app/pages/projects/ProjectsPage.page.jsx
+++ client-app/src/app/pages/projects/ProjectsPage.page.jsx
@@ -3,10 +3,12 @@ import { Fragment } from "react";
 import { projects } from "../../utils/projectsData";
 import ProjectCard from "../../components/project-card/ProjectCard.component";
 
+import "./ProjectsPage.styles.scss";
+
 const ProjectsPage = () => {
   return (
     <Fragment>
-      <div className="projects-page-container d-flex justify-content-center align-items-center flex-wrap gap-4 py-5 mt-5 px-0 mb-5">
+      <div className='projects-page-container d-flex justify-content-center align-items-center flex-wrap flex-column gap-4 py-5 mt-5 px-0'>
         {projects.map((project, index) => {
           return (
             <Fragment key={index}>
